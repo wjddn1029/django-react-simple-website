7b52a901cb0b70e69e6cf5305cf383183c455f7b
wjddn1029 <wjddn1029@kakao.com>
2022-09-07 23:49:13 +0900

    [accounts] views 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,47 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model, login as auth_login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, CreateView
 
+# @login_required
+# def profile(request):
+#     return render(request, 'accounts/profile.html')
+from accounts.forms import ProfileForm
+from accounts.models import Profile
+
+
 User = get_user_model()
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile.html'
+
+
+profile = ProfileView.as_view()
+
+
+@login_required
+def profile_edit(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = None
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'accounts/profile_form.html', {
+        'form': form,
+    })
 
 
 class SignupView(CreateView):
