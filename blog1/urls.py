8bc5824733b1d47c5fe413583e644263033acafe
wjddn1029 <wjddn1029@kakao.com>
2022-10-06 23:49:25 +0900

    [blog1] url 오류 수정

--- blog1/urls.py
+++ blog1/urls.py
@@ -1,22 +1,9 @@
-"""askcompany URL Configuration
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from . import views
+
+app_name = 'blog1'  # URL Reverse에서 namespace역할을 하게 됩니다.
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog1/', include('blog1.urls')),
+    path('', views.post_list, name='post_list'),
 ]
