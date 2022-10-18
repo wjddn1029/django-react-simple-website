9b2d3ab4ed42793e649013a2d3cf5688918018d9
wjddn1029 <wjddn1029@kakao.com>
2022-10-18 17:33:58 +0900

    [instagram] admin 수정/추가

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from django.utils.safestring import mark_safe
+
+from .models import Post, Comment, Tag
 
 
-admin.site.register(Post)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
