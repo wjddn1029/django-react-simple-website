bf25233ca699ce6ceceb7c50280a86e6b5fc214b
wjddn1029 <wjddn1029@kakao.com>
2022-10-20 17:37:19 +0900

    [instagram] models post push

---
+++ instagram/models.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.core.validators import MinLengthValidator
+from django.db import models
+from django.urls import reverse
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField(
+        validators=[MinLengthValidator(10)]
+    )
+    photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d')
+    tag_set = models.ManyToManyField('Tag', blank=True)
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Java의 toString
+    def __str__(self):
+        # return f"Custom Post object ({self.id})"
+        # return "Custom Post object ({})".format(self.id)
+        return self.message
+
+    def get_absolute_url(self):
+        return reverse('instagram:post_detail', args=[self.pk])
