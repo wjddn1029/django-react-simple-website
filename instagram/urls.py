4905ea47139adc9bc9eb2feeae13cb38b9fec4e8
wjddn1029 <wjddn1029@kakao.com>
2022-10-30 23:25:21 +0900

    year/month/day converter 추가, url 추가

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, re_path, register_converter
 
 from . import views
+from .converters import YearConverter, MonthConverter, DayConverter
+
+register_converter(YearConverter, 'year')
+register_converter(MonthConverter, 'month')
+register_converter(DayConverter, 'day')
 
 
 app_name = 'instagram'  # URL Reverse에서 namespace역할을 하게 됩니다.
@@ -9,5 +14,11 @@ urlpatterns = [
     path('new/', views.post_new, name='post_new'),
     path('<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('<int:pk>/delete/', views.post_delete, name='post_delete'),
+
+    path('', views.post_list, name='post_list'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+
+    path('archive/', views.post_archive, name='post_archive'),
+    path('archive/<year:year>/', views.post_archive_year, name='post_archive_year'),
 
 ]
